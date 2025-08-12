ce30c90aff5816bf3e9ccba1236efbfad4eedb0d
Eliran Sharabi <esharabi@salesforce.com>
2025-08-12 19:00:28 +0300

    Initial Commit

---
+++ NutritionCounter/NutritionCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionCounterApp.swift
+//  NutritionCounter
+//
+//  Created by Eliran Sharabi on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NutritionCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
